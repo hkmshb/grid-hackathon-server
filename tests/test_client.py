39605e88fe0e09db0913564d83295292937955e3
Abdul-Hakeem Shaibu <hkmshb@gmail.com>
2018-11-15 17:37:43 +0100

    add CQLBuilder to build CQL Filters from query params

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,5 +1,8 @@
-from grid3_hackserver.client import APIClient, GeoServerAPIClient
+import pytest
+from grid3_hackserver.common import APIClient, GeoServerAPIClient, CQLBuilder
 
+
+cql_builder = CQLBuilder()
 
 
 class TestClients:
@@ -13,3 +16,35 @@ class TestClients:
         gsclient = GeoServerAPIClient('https://localhost:5000', '7782-038e2')
         f = gsclient._build_url('schools', **{'gender': 'M'})
         assert '&authkey=7782-038e2' in f.url
+
+
+class TestQueryInterpreter:
+
+    @pytest.mark.parametrize('args, expected', [
+        (("state_code", "'KN'"), "state_code = 'KN'" ),
+        (("client_age", "3"), "client_age = 3")
+    ])
+    def test_parse_simple_expr(self, args, expected):
+        cond = cql_builder._parse_expr_simple(*args)
+        assert cond == expected
+
+    @pytest.mark.parametrize('args, expected', [
+        (("state_code__like", "'KA%'"), "state_code LIKE 'KA%'"),
+        (("state_name__ilike", "'KA%'"), "state_name ILIKE 'KA%'"),
+        (("client_age__lt", "20"), "client_age < 20"),
+        (("client_age__le", "20"), "client_age <= 20"),
+        (("client_age__gt", "20"), "client_age > 20"),
+        (("client_age__ge", "20"), "client_age >= 20"),
+        (("client_age__in", "20,23,24"), "client_age IN (20, 23, 24)"),
+        (("client_age__between", "20,25"), "client_age BETWEEN 20 AND 25"),
+    ])
+    def test_parse_complex_expr(self, args, expected):
+        cond = cql_builder._parse_expr_complex(*args)
+        assert cond == expected
+
+    def test_parse(self):
+        cql = cql_builder({
+            "state_code__like": "'KA%'",
+            "client_age__in": "20,55,73"
+        })
+        assert cql == "state_code LIKE 'KA%' AND client_age IN (20, 55, 73)"
