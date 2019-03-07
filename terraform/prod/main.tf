021e22db86562e54aa19b1d06f4850d5f886854b
Oluwafemi Olofintuyi <oluwafemi.t.olofintuyi@gmail.com>
2019-03-07 21:04:10 +0100

    use cluster alb

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -1,8 +1,10 @@
 module "ecs" {
-  source               = "git@github.com:eHealthAfrica/terraform//generic_ecs_service?ref=v2.0.2"
+  source               = "git@github.com:eHealthAfrica/terraform//generic_ecs_service"
   environment           = "${var.environment}"
   project               = "${var.project}"
+  project_billing_id    = "${var.project_billing_id}"
   app                   = "api"
+  http_rule_priority    = 804
   database_hostname     = "not_required"
   application_memory    = 4096
   memory_reservation    = 1024
